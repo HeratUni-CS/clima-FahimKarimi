317faa22ef9f751e3df9e1caf7f4551bcf9308bf
FahimKarimi <fahimkarimi2009@gmail.com>
2023-06-10 00:33:30 +0430

    new

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,3 +1,25 @@
+import 'package:clima/services/location.dart';
+import 'networking.dart';
+
+const apiKey = 'dd558343d3f6126f2adffdf1c28522b1';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    return await networkHelper.getData();
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+    return await networkHelper.getData();
+  }
+  
 class WeatherModel {
   String getWeatherIcon(int condition) {
     if (condition < 300) {
